1345a3728e3559309bd2f7dded38a8d6d3f5460b
태현 이 <330529@naver.com>
2022-07-29 18:01:33 +0900

    경로참조기능 추가

--- DartApI/Form1.Designer.cs
+++ DartApI/Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.고유넘버 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.기업명 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.테스트 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,17 +52,59 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.고유넘버,
+            this.기업명,
+            this.테스트,
+            this.Column1,
+            this.Column2});
             this.dataGridView1.Location = new System.Drawing.Point(51, 41);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(631, 364);
             this.dataGridView1.TabIndex = 1;
             // 
+            // 고유넘버
+            // 
+            this.고유넘버.HeaderText = "고유번호";
+            this.고유넘버.Name = "고유넘버";
+            // 
+            // 기업명
+            // 
+            this.기업명.HeaderText = "기업명";
+            this.기업명.Name = "기업명";
+            // 
+            // 테스트
+            // 
+            this.테스트.HeaderText = "테스트";
+            this.테스트.Name = "테스트";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(703, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +118,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 고유넘버;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 기업명;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 테스트;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DartApI/Form1.cs
+++ DartApI/Form1.cs
@@ -27,43 +27,63 @@ namespace DartApI
             using (WebClient webClient = new WebClient())
             {
                 var jason = webClient.DownloadString("https://opendart.fss.or.kr/api/corpCode.xml");
-
-                MessageBox.Show(jason.ToString());
+                webClient.OpenRead("https://opendart.fss.or.kr/api/corpCode.xml");
+              
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-            ReadXML();
+         
 
 
         }
         /// <summary>
         /// xml파싱
         /// </summary>
-        public void ReadXML()
+        public void ReadXML(string path)
         {
             string temp = "";
+        
 
             XmlDocument xml = new XmlDocument();
 
-            xml.Load("C:\\Users\\cit\\Desktop\\corpCode\\CORPCODE.xml");
+            xml.Load(path);
+            //ds.ReadXmlSchema("C:\\Users\\cit\\Desktop\\corpCode\\CORPCODE.xml");
+            //ds.ReadXml("C:\\Users\\cit\\Desktop\\corpCode\\CORPCODE.xml");
 
             XmlNodeList xmlList = xml.SelectNodes("/result/list"); //xml노드 셀렉 result 노드의 list노드들을 가져옴                     
 
+
             foreach (XmlNode xnl in xmlList)
             {
-                temp += xnl["corp_code"].InnerText;
-                temp += xnl["corp_name"].InnerText;
-                temp += xnl["stock_code"].InnerText;
-                temp += xnl["modify_date"].InnerText;
-                MessageBox.Show(temp);
-                temp = null;
-
+                if(!string.IsNullOrEmpty(xnl["stock_code"].InnerText))
+                dataGridView1.Rows.Add(xnl["corp_code"].InnerText.ToString()                                    
+                                     , xnl["corp_name"].InnerText.ToString()
+                                     , xnl["stock_code"].InnerText.ToString()
+                                     , xnl["modify_date"].InnerText.ToString());
+                //temp += xnl["corp_code"].InnerText;
+                //temp += xnl["corp_code"].InnerText;
+                //temp += xnl["corp_name"].InnerText;
+                //temp += xnl["stock_code"].InnerText;
+                //temp += xnl["modify_date"].InnerText;
+                //MessageBox.Show(temp);
+                //temp = null;
             }
 
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                                                            //초기 기업정보 xml 경로 세팅 로직
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();                            //파일찾는 화면 표기
+            dialog.InitialDirectory = "C:\\\\";             //기본 경로 c드라이브 설정
+            string path = dialog.FileName;                  //선택파일 경로 세팅
+
+            ReadXML(path);                                  //xml가공 메서드에 경로 전달
         }
     }
 }
